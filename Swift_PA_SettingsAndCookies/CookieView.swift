2bdb97bf31fe9c9dc06fc7666d7ac0ca4758a508
MarcelL1989 <marcelludwig89@icloud.com>
2023-03-03 10:58:06 +0100

    Aufgabe 1 erledigt

--- Swift_PA_SettingsAndCookies/CookieView.swift
+++ Swift_PA_SettingsAndCookies/CookieView.swift
@@ -9,8 +9,44 @@ import SwiftUI
 
 //MARK: CookieView
 struct CookieView: View {
+    @State var counter : Int = 0
+    @State var positionX: CGFloat = 0
+    @State var positionY: CGFloat = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+        
+        
+        VStack {
+            Text("Cookie Count : \(counter)")
+            Button(action:  {
+                countUp()
+                position()
+                
+                
+                
+                
+            }) {
+                Image("CookieImage")
+                    .resizable()
+                    .frame(width: 60,height: 60)
+                    
+            }.position(x:positionX,y:positionY)
+
+                
+           
+        }
+    }
+    func countUp() {
+        counter = counter+1
+        
+        
+    }
+    func position() {
+        var x = Array(0...300)
+        var y = Array(0...600)
+        positionX = CGFloat(x.randomElement()!)
+        positionY = CGFloat(y.randomElement()!)
+        
     }
 }
 
